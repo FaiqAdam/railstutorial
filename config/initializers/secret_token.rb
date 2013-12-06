69e76123cfd0dd2f8f6746323b7bd3fdc73065a7
Faiq Adam <faiq.adam@gmail.com>
2013-12-06 11:42:35 +0530

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial::Application.config.secret_token = '40e0a15781f574548eafce85e3fba3c3f0007c9f9f0d9b46694c98a9f9ba0899754efd763d0776bf9709a175be400089e1897ab529734124b56db25550208686'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Tutorial::Application.routes.draw do
+  devise_for :users
+
+  resources :posts
+  root :to=> 'posts#index'
+end
